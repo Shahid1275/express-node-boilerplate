a30864935d81286b19232568d9b11e33ee47b8f1
vuswh <vuswh@LRO-SH-20.vu.edu.pk>
2025-04-28 16:11:26 +0500

    updated code

--- url-shortner/config/db.js
+++ url-shortner/config/db.js
@@ -8,13 +8,11 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
         });
         console.log("MongoDB connected successfully!");
+        return conn;
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-    mongoose.connection.on("error", (err) => {
-        console.log("MongoDB connection error:", err);
-    });
 };
 
 export default connectDB;
